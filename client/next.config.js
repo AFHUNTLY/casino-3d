5d30451beb0193ad4ee85e6970c953aeaec24227
AFHUNTLY <arefrahmani1385@gmail.com>
2026-07-05 12:35:13 +0000

    Fix: remove static export for Vercel public/ serving

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
-  // Serve static files from public/
-  webpack: (config) => {
-    return config;
-  },
 };
 
 module.exports = nextConfig;
